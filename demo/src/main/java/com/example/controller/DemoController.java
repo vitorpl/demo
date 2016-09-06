e9d7012cdaae9d5af690fc1bd00e981c6a49dbc9
Vitor Pastuch Lazarotto <vitor.lazarotto@gmail.com>
2016-09-05 23:50:21 -0300

    Primeiro commit

---
+++ demo/src/main/java/com/example/controller/DemoController.java
@@ -0,0 +1,14 @@
+package com.example.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/demo")
+public class DemoController {
+
+	@RequestMapping
+	public String main() {
+		return "/index";
+	}
+}
